d4f911a212ada033cd1d1e2e48c1f4351b720746
David Cawton II <Dvaidc@Davids-MacBook-Pro.local>
2015-11-18 15:46:46 -0600

    Fixed JS

--- src/images.js
+++ src/images.js
@@ -1,44 +1,48 @@
 export default function() {
 
   var toggleForm = function() {
-    $(`.form-section`). slideToggle();
+    $('.form-section'). slideToggle();
   };
 
-  var submitButton = $(`.add-img-btn`);
-  var cancel = $(`.cancel`);
+  var submitButton = $('.add-img-btn');
+  var cancel = $('.cancel');
 
-  var showContact = function(contact) {
-    $(`<img></img><p></p>`)
-    .text(`${image.url} - ${image.description}`)
-    .appendTo(`.images`);
+  var showContact = function(image) {
+    $('<div></div>')
+      .addClass('images')
+      .appendTo('.images')
+      .html(`${image.image} - ${image.text}`);
   };
 
-  $(`.fa-plus-circle`).on(`click`, toggleForm);
+  $('.fa-plus-circle').on('click', toggleForm);
 
-  $(`.form-section`).on(`submit`, function(ev) {
+  $('.form-section').on('submit', function(ev) {
     ev.preventDefault();
 
-    var image = $(`.image-url`).val();
-    var text = $(`.image-caption`).val();
+    var image = $('.image-url').val();
+    var text = $('.image-caption').val();
 
     $.ajax({
-      url: `http://tiny-lr.herokuapp.com/collections/contacts-dc`,
-      method: `POST`,
-      dataType: `json`,
+      url: 'http://tiny-lr.herokuapp.com/collections/contacts-dc',
+      method: 'POST',
+      dataType: 'json',
       data: {image, text},
     }).then((response) => {
-      $(`.image-url`).val('');
-      $(`.image-caption`).val('');
+      $('.image-url').val('');
+      $('.image-caption').val('');
       toggleForm();
 
       showContact(response);
     });
-
-    $.ajax({
-      url: `http://tiny-lr.herokuapp.com/collections/contacts-dc`,
-      method: `GET`,
-      dataType: `json`,
-      data: {image, text},
-    }).forEach(showContact);
   });
-};
+
+  $.ajax({
+    url: 'http://tiny-lr.herokuapp.com/collections/contacts-dc',
+    method: 'GET',
+    dataType: 'json',
+  }).then((allImages) => {
+    allImages.forEach(showContact);
+  });
+}
+
+// npm install -g yo generator-backbone-broccoli
